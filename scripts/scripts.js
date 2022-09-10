ca4f1a539fede3ad6d97c8519c23432e81330397
AlaaAridi <112479275+AlaaAridi@users.noreply.github.com>
2022-09-11 02:17:43 +0300

    three fetch functions working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,31 @@
 // Creating Constants
 
-let userName = document.getElementById("user-name").value;
+let userName = document.getElementById("user-name");
 const predict = document.getElementById("predict");
-predict.addEventListener("click", getAge);
+predict.addEventListener("click", getGen);
+
+
+function getNat() {
+    let userInput = document.getElementById("user-name").value.trim();
+    url = "https://api.nationalize.io/?name=" + userInput;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+}
 
 
 function getAge() {
-    let userInput = document.getElementById('user-name').value.trim();
-    url = "https://api.nationalize.io/?name=" + userInput;
+    let userInput = document.getElementById("user-name").value.trim();
+    url = "https://api.agify.io/?name=" + userInput;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+}
+
+
+function getGen() {
+    let userInput = document.getElementById("user-name").value.trim();
+    url = "https://api.genderize.io/?name=" + userInput;
     fetch(url)
         .then((response) => response.json())
         .then((data) => console.log(data))
