f99e3e70288273c3c5ecc84573140fed8f88b58d
AlaaAridi <112479275+AlaaAridi@users.noreply.github.com>
2022-09-11 01:24:26 +0300

    first win fetch succeed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,34 +1,14 @@
 // Creating Constants
 
-/*const dogImg = document.querySelectorAll(".dog-img");
-
-fetch("https://dog.ceo/api/breeds/image/random", {
-
-})
-
-
-fetch("https://api.nationalize.io/?name=mohamad")
-    
-
-img.src = data.message
-
-async function getUsers() {
-    let url = "https://api.genderize.io/?name=rio";
-    try {
-        let res = await fetch("https://api.genderize.io/?name=rio");
-        return await res.json();
-    } catch (error) {
-        console.log(error);
-    }
-}
-*/
-
-let name = document.getElementById("name").value;
+let userName = document.getElementById("user-name").value;
 const predict = document.getElementById("predict");
 predict.addEventListener("click", getAge);
 
+
 function getAge() {
-    fetch('https://api.agify.io/?name=${name}')
+    let userInput = document.getElementById('user-name').value.trim();
+    url = "https://api.nationalize.io/?name=" + userInput;
+    fetch(url)
         .then((response) => response.json())
         .then((data) => console.log(data))
 }
